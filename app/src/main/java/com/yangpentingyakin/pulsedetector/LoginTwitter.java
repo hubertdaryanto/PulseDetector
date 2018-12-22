11bc9bd0a00e41191fb2db3e86aa4e64a1eb280d
lusiana M <williamhans27.mb1@gmail.com>
2018-12-22 10:44:04 +0700

    update

---
+++ app/src/main/java/com/yangpentingyakin/pulsedetector/LoginTwitter.java
@@ -0,0 +1,13 @@
+package com.yangpentingyakin.pulsedetector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginTwitter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_twitter);
+    }
+}
