ead93c9999b245a94c96e339fa2b0ab6d71ac78f
lusiana M <williamhans27.mb1@gmail.com>
2018-12-22 10:49:01 +0700

    update

--- app/src/main/java/com/yangpentingyakin/pulsedetector/MainActivity.java
+++ app/src/main/java/com/yangpentingyakin/pulsedetector/MainActivity.java
@@ -39,8 +39,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        buttonON=(Button) findViewById(R.id.btON);
-        buttonOFF=(Button) findViewById(R.id.btOFF);
+      //  buttonON=(Button) findViewById(R.id.btON);
+      //  buttonOFF=(Button) findViewById(R.id.btOFF);
         myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         bluetoothONMethod();
@@ -76,11 +76,11 @@ public class MainActivity extends AppCompatActivity
                 }
                 else
                 {
-                    if(!myBluetoothAdapter.isEnabled())
-                    {
+            //        if(!myBluetoothAdapter.isEnabled())
+              //      {
 
 
-                    }
+                //    }
                 }
             }
         });
@@ -144,78 +144,78 @@ public class MainActivity extends AppCompatActivity
 
 
 //bluetooth systems
-
-
-
-
-
-    //013
-    Handler handler = new Handler(new Handler.Callback() {
-        @Override
-        public boolean handleMessage(Message msg) {
-            switch (msg.what) {
-                case STATE_LISTENING:
-                    status.setText("Listening");
-                    break;
-                case STATE_CONNECTING:
-                    status.setText("Connecting");
-                    break;
-                case STATE_CONNECTED:
-                    status.setText("Connected");
-                    break;
-                case STATE_CONNECTION_FAILED:
-                    status.setText("Connection Failed");
-                    break;
-                case STATE_MESSAGE_RECEIVED:
-                    byte[] readBuff = (byte[]) msg.obj;
-                    String tempMsg = new String(readBuff, 0, msg.arg1);
-                    msg_box.setText(tempMsg);
-                    break;
-            }
-            return true;
-        }
-    });
-
-    private class SendReceive extends Thread {
-        private final BluetoothSocket bluetoothSocket;
-        private final InputStream inputStream;
-        private final OutputStream outputStream;
-
-        public SendReceive(BluetoothSocket socket) {
-            bluetoothSocket = socket;
-            InputStream tempIn = null;
-            OutputStream tempOut = null;
-
-            try {
-                tempIn = bluetoothSocket.getInputStream();
-                tempOut = bluetoothSocket.getOutputStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            inputStream = tempIn;
-            outputStream = tempOut;
-        }
-    }
-
-    public void run() {
-        byte[] buffer = new byte[1024];
-        int bytes;
-
-        while (true) {
-            try {
-                bytes = inputStream.read(buffer);
-                handler.obtainMessage(STATE_MESSAGE_RECEIVED, bytes, -1, buffer).sendToTarget();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void write(byte[] bytes) {
-        try {
-            outputStream.weite(bytes);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//
+//
+//
+//
+//
+//    //013
+//    Handler handler = new Handler(new Handler.Callback() {
+//        @Override
+//        public boolean handleMessage(Message msg) {
+//            switch (msg.what) {
+//                case STATE_LISTENING:
+//                    status.setText("Listening");
+//                    break;
+//                case STATE_CONNECTING:
+//                    status.setText("Connecting");
+//                    break;
+//                case STATE_CONNECTED:
+//                    status.setText("Connected");
+//                    break;
+//                case STATE_CONNECTION_FAILED:
+//                    status.setText("Connection Failed");
+//                    break;
+//                case STATE_MESSAGE_RECEIVED:
+//                    byte[] readBuff = (byte[]) msg.obj;
+//                    String tempMsg = new String(readBuff, 0, msg.arg1);
+//                    msg_box.setText(tempMsg);
+//                    break;
+//            }
+//            return true;
+//        }
+//    });
+//
+//    private class SendReceive extends Thread {
+//        private final BluetoothSocket bluetoothSocket;
+//        private final InputStream inputStream;
+//        private final OutputStream outputStream;
+//
+//        public SendReceive(BluetoothSocket socket) {
+//            bluetoothSocket = socket;
+//            InputStream tempIn = null;
+//            OutputStream tempOut = null;
+//
+//            try {
+//                tempIn = bluetoothSocket.getInputStream();
+//                tempOut = bluetoothSocket.getOutputStream();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            inputStream = tempIn;
+//            outputStream = tempOut;
+//        }
+//    }
+//
+//    public void run() {
+//        byte[] buffer = new byte[1024];
+//        int bytes;
+//
+//        while (true) {
+//            try {
+//                bytes = inputStream.read(buffer);
+//                handler.obtainMessage(STATE_MESSAGE_RECEIVED, bytes, -1, buffer).sendToTarget();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    public void write(byte[] bytes) {
+//        try {
+//            outputStream.weite(bytes);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
