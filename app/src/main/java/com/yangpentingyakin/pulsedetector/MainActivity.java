0fac72758a2bab4d1a5eeb35b0d3aed672208af0
Hubert <partishion@gmail.com>
2019-01-04 20:51:48 +0700

    value dari  arduino belum benar

--- app/src/main/java/com/yangpentingyakin/pulsedetector/MainActivity.java
+++ app/src/main/java/com/yangpentingyakin/pulsedetector/MainActivity.java
@@ -39,24 +39,22 @@ public class MainActivity extends AppCompatActivity
 
     private Button Login;
     //    private final String DEVICE_NAME="MyBTBee";
-    private final String DEVICE_ADDRESS="20:13:10:15:33:66";
+    private final String DEVICE_ADDRESS="FC:A8:9A:00:83:1B";
     private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
     private BluetoothDevice device;
     private BluetoothSocket socket;
     private OutputStream outputStream;
     private InputStream inputStream;
     Button startButton, sendButton,clearButton,stopButton;
-    TextView textView;
+    TextView DenyutTest;
     EditText editText;
     boolean deviceConnected=false;
     Thread thread;
     byte buffer[];
-    int bufferPosition;
+    int bytes;
     boolean stopThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Login = (Button) findViewById(R.id.LoginButton);
@@ -92,7 +90,19 @@ public class MainActivity extends AppCompatActivity
                 startActivity(intent);
             }
         });
-        textView = (TextView) findViewById(R.id.textView);
+        DenyutTest =(TextView) findViewById(R.id.BacaDenyut);
+
+        if(BTinit())
+        {
+            if(BTconnect())
+            {
+
+                deviceConnected=true;
+                beginListenForData();
+                DenyutTest.append("\nConnection Opened!\n");
+            }
+
+        }
 
 
     }
@@ -109,7 +119,7 @@ public class MainActivity extends AppCompatActivity
             Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableAdapter, 0);
             try {
-                Thread.sleep(1000);
+                Thread.sleep(30);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -163,19 +173,6 @@ public class MainActivity extends AppCompatActivity
         return connected;
     }
 
-    public void onClickStart(View view) {
-        if(BTinit())
-        {
-            if(BTconnect())
-            {
-
-                deviceConnected=true;
-                beginListenForData();
-                textView.append("\nConnection Opened!\n");
-            }
-
-        }
-    }
 
     void beginListenForData()
     {
@@ -199,11 +196,13 @@ public class MainActivity extends AppCompatActivity
                             handler.post(new Runnable() {
                                 public void run()
                                 {
-                                    textView.append(string);
+                                    DenyutTest.setText(string);
                                 }
                             });
 
                         }
+
+
                     }
                     catch (IOException ex)
                     {
@@ -215,11 +214,6 @@ public class MainActivity extends AppCompatActivity
 
         thread.start();
     }
-
-
-
-
-
 
 
     @Override
@@ -296,85 +290,4 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-
-
-
-    //serial input
-
-
-//bluetooth chat systems
-//
-//
-//
-//
-//
-//    //013
-//    Handler handler = new Handler(new Handler.Callback() {
-//        @Override
-//        public boolean handleMessage(Message msg) {
-//            switch (msg.what) {
-//                case STATE_LISTENING:
-//                    status.setText("Listening");
-//                    break;
-//                case STATE_CONNECTING:
-//                    status.setText("Connecting");
-//                    break;
-//                case STATE_CONNECTED:
-//                    status.setText("Connected");
-//                    break;
-//                case STATE_CONNECTION_FAILED:
-//                    status.setText("Connection Failed");
-//                    break;
-//                case STATE_MESSAGE_RECEIVED:
-//                    byte[] readBuff = (byte[]) msg.obj;
-//                    String tempMsg = new String(readBuff, 0, msg.arg1);
-//                    msg_box.setText(tempMsg);
-//                    break;
-//            }
-//            return true;
-//        }
-//    });
-//
-//    private class SendReceive extends Thread {
-//        private final BluetoothSocket bluetoothSocket;
-//        private final InputStream inputStream;
-//        private final OutputStream outputStream;
-//
-//        public SendReceive(BluetoothSocket socket) {
-//            bluetoothSocket = socket;
-//            InputStream tempIn = null;
-//            OutputStream tempOut = null;
-//
-//            try {
-//                tempIn = bluetoothSocket.getInputStream();
-//                tempOut = bluetoothSocket.getOutputStream();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            inputStream = tempIn;
-//            outputStream = tempOut;
-//        }
-//    }
-//
-//    public void run() {
-//        byte[] buffer = new byte[1024];
-//        int bytes;
-//
-//        while (true) {
-//            try {
-//                bytes = inputStream.read(buffer);
-//                handler.obtainMessage(STATE_MESSAGE_RECEIVED, bytes, -1, buffer).sendToTarget();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//
-//    public void write(byte[] bytes) {
-//        try {
-//            outputStream.weite(bytes);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
